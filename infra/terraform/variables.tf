30860864811db19c85702741cc46607efcab9b07
sonam-niit <sonam.gravity@gmail.com>
2026-02-21 19:55:38 +0530

    variable  updated

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,7 +1,6 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
-  default = "us-east-1"
-  type = string
+  default     = "us-east-1"
 }
 variable "upload_bucket_name" {
   description = "Name of the S3 bucket to upload files"
