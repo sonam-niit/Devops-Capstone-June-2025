4787265d891996996893fb0a6a1faf4de5d3d4ce
sonam-niit <sonam.gravity@gmail.com>
2026-02-21 19:51:49 +0530

    SNS updated

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -340,3 +340,37 @@ resource "aws_lambda_permission" "allow_apigw_invoke_presign" {
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.presign_api.execution_arn}/*/*"
 }
+
+# -----------------------------
+# SNS Topic for Notifications
+# -----------------------------
+
+resource "aws_sns_topic" "devops_accelerator_upload_notify" {
+  name = "devops-accelerator-upload-notification-topic"
+}
+
+resource "aws_sns_topic_subscription" "devops_accelerator_email_sub" {
+  topic_arn = aws_sns_topic.devops_accelerator_upload_notify.arn
+  protocol  = "email"
+  endpoint  = var.notification_email
+}
+
+resource "aws_iam_policy" "devops_accelerator_lambda_sns_policy" {
+  name = "devops-accelerator-lambda-sns-publish-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "sns:Publish",
+        Resource = aws_sns_topic.devops_accelerator_upload_notify.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_sns_policy_attachment" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.devops_accelerator_lambda_sns_policy.arn
+}
